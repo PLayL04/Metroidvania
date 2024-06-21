884c9e5c6d1fe1a39a05bc1c4e1800ecccd0ebf9
PLayL04 <playl04@yandex.ru>
2024-06-21 11:48:57 +0700

    Additive Scene Loader

---
+++ Assets/Scripts/Controllers/SceneLoader.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Transform player;
+
+    //Scene state
+    private bool isLoaded;
+    private bool shouldLoad;
+    void Start()
+    {
+        //verify if the scene is already open to avoid opening a scene twice
+        if (SceneManager.sceneCount > 0)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if (scene.name == gameObject.name)
+                {
+                    isLoaded = true;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        TriggerCheck();
+    }
+
+    void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            //Loading the scene, using the gameobject name as it's the same as the name of the scene to load
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            //We set it to true to avoid loading the scene twice
+            isLoaded = true;
+        }
+    }
+
+    void UnLoadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = false;
+        }
+    }
+
+    void TriggerCheck()
+    {
+        if (shouldLoad)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+}
